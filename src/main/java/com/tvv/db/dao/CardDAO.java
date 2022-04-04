9fbd26c0b1bf54f15da3280ba15059887c2d0f00
tymchuk.volodymyr <tymchuk.volodymyr@gmail.com>
2022-04-04 16:49:12 +0300

    comments for CardDAO

--- src/main/java/com/tvv/db/dao/CardDAO.java
+++ src/main/java/com/tvv/db/dao/CardDAO.java
@@ -8,6 +8,9 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * DAO functions for CRUD operation Card
+ */
 public class CardDAO {
     private static final String SQL__FIND_ALL_CARDS =
             "SELECT * FROM cards";
@@ -30,6 +33,12 @@ public class CardDAO {
             "insert into cards (id, name, number, expDate, user_id, statusCard) "+
             "values (default,?,?,?,?,?)";
 
+    /**
+     * Insert card to DB
+     * @param card object Card
+     * @return object Card
+     * @throws AppException
+     */
     public static Card insertCard (Card card) throws AppException {
         PreparedStatement pstmt = null;
         Connection con = null;
@@ -54,6 +63,11 @@ public class CardDAO {
         return card;
     }
 
+    /**
+     * Find all cards in DB
+     * @return List of Cards
+     * @throws AppException
+     */
     public static List<Card> findAllCards() throws AppException {
         List<Card> cards = new ArrayList<>();
         Statement stmt = null;
@@ -78,6 +92,12 @@ public class CardDAO {
         return cards;
     }
 
+    /**
+     * Find card by id
+     * @param id Card id
+     * @return object Card
+     * @throws AppException
+     */
     public static Card findCardById(Long id) throws AppException {
         Card card = new Card();
         PreparedStatement pstmt = null;
@@ -103,14 +123,33 @@ public class CardDAO {
         return card;
     }
 
+    /**
+     * Find card by account. Not used, will be developed in the future
+     * @param accountId account id
+     * @return List of Cards (can be changed in the future)
+     * @throws AppException
+     */
     public static List<Card> findCardByAccount(Long accountId) throws AppException {
         return getCards(accountId, SQL__FIND_CARDS_BY_ACCOUNT);
     }
 
+    /**
+     * FInd cards by User id
+     * @param userId User id
+     * @return List of card, owner - user by id
+     * @throws AppException
+     */
     public static List<Card> findCardsByUser(Long userId) throws AppException {
         return getCards(userId, SQL__FIND_CARDS_BY_USER_ID);
     }
 
+    /**
+     * Function for find card
+     * @param userId - object id
+     * @param sqlRequest - SQL request for find Cards
+     * @return List of Cards
+     * @throws AppException
+     */
     private static List<Card> getCards(Long userId, String sqlRequest) throws AppException {
         List<Card> cards = new ArrayList<>();
         PreparedStatement pstmt = null;
@@ -135,6 +174,13 @@ public class CardDAO {
         return cards;
     }
 
+    /**
+     * Update card status by card id
+     * @param id Card id
+     * @param newStatus new status
+     * @return successful operation
+     * @throws AppException
+     */
     public static boolean updateStatusCardById(Long id, int newStatus) throws AppException {
         boolean result = false;
         PreparedStatement pstmt = null;
@@ -156,8 +202,16 @@ public class CardDAO {
         return result;
     }
 
+    /**
+     * Class for load object from DB
+     */
     private static class CardLoad implements LoadEntity<Card> {
-
+        /**
+         * Load object from ResultSet
+         * @param rs ResultSet
+         * @return Card object
+         * @throws AppException
+         */
         @Override
         public Card loadRow(ResultSet rs) throws AppException {
             try {
