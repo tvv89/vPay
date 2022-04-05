5536ee43eb6974cbb464d36d02361a5e376ce185
tymchuk.volodymyr <tymchuk.volodymyr@gmail.com>
2022-04-05 22:08:13 +0300

    comments for SystemParameters

--- src/main/java/com/tvv/utils/SystemParameters.java
+++ src/main/java/com/tvv/utils/SystemParameters.java
@@ -1,8 +1,20 @@
 package com.tvv.utils;
 
+/**
+ * System parameters
+ */
 public class SystemParameters {
+    /**
+     * Payment commission for outside recipient
+     */
     public static final Double COMMISSION_PERCENT = 0.05;
+    /**
+     * Min sum of payment
+     */
     public static final Double MIN_PAYMENT_SUM = 1D;
+    /**
+     * Max sum of payment
+     */
     public static final Double MAX_PAYMENT_SUM = 100000D;
 
 }
