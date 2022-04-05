7542574919953966b9123af111e225cf4cf158f7
tymchuk.volodymyr <tymchuk.volodymyr@gmail.com>
2022-04-05 20:11:02 +0300

    comments for AppException

--- src/main/java/com/tvv/service/exception/AppException.java
+++ src/main/java/com/tvv/service/exception/AppException.java
@@ -1,6 +1,14 @@
 package com.tvv.service.exception;
 
+/**
+ * Application exception class
+ */
 public class AppException extends Exception{
+    /**
+     * Custom exception for web application
+     * @param message custom message
+     * @param exception cause exception
+     */
     public AppException(String message, Exception exception)
     {
         super(message,exception);
