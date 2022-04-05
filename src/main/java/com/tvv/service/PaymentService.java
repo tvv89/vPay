92cc7f4064b8b3e52f5051e76469986275c9eaa9
tymchuk.volodymyr <tymchuk.volodymyr@gmail.com>
2022-04-05 20:53:23 +0300

    comments for PaymentService

--- src/main/java/com/tvv/service/PaymentService.java
+++ src/main/java/com/tvv/service/PaymentService.java
@@ -15,30 +15,64 @@ import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 
-
+/**
+ * Business logic for Payment
+ */
 public class PaymentService {
 
+
+    /**
+     * Currency
+     */
     private enum CurrencyExchange {
+        /**
+         * Euro with coefficient to USD
+         */
         EUR (1.15),
+        /**
+         * Dollar with coefficient to USD
+         */
         USD (1),
+        /**
+         * Gryvnya with coefficient to USD
+         */
         UAH (0.033);
 
         private final double rate;
 
+        /**
+         * @param k coefficient of currency
+         */
         CurrencyExchange(double k) {
             this.rate = k;
         }
 
+        /**
+         * Get coefficient by enum value
+         * @return double value of coefficient
+         */
         public double getRate() {
             return rate;
         }
 
     }
 
+    /**
+     * Get coefficient of enum by name
+     * @param name
+     * @return
+     */
     public static double getRateByName(String name) {
         return CurrencyExchange.valueOf(name).getRate();
     }
 
+    /**
+     * Currency exchange
+     * @param value value of currency from
+     * @param currencyFrom currency from (string name of enum)
+     * @param currencyTo currency to (string name of enum)
+     * @return result value of currency to
+     */
     public static double currencyExchange (Double value, String currencyFrom, String currencyTo) {
         double res = value*(getRateByName(currencyFrom)/getRateByName(currencyTo));
         DecimalFormat df = new DecimalFormat("#.##",new DecimalFormatSymbols(Locale.ENGLISH));
@@ -46,11 +80,22 @@ public class PaymentService {
         return res;
     }
 
+    /**
+     * Create payment function
+     * @param payment object Payment
+     * @return successful operation
+     * @throws AppException
+     */
     public static boolean createPayment(Payment payment) throws AppException {
-
         return PaymentDAO.insertPayment(payment);
     }
 
+    /**
+     * Change status payment and deposit account/s
+     * @param payment current payment
+     * @return JSON result request with status of payment
+     * @throws AppException
+     */
     public static JsonObject changeStatusPayment(Payment payment) throws AppException {
         JsonObject innerObject = new JsonObject();
         if ("Ready".equals(payment.getStatus())) {
@@ -88,6 +133,12 @@ public class PaymentService {
         return UtilCommand.errorMessageJSON("Payment can not be submitted.");
     }
 
+    /**
+     * Function for "delete" payment, go to archive
+     * @param payment current payment
+     * @return JSON result request with status operation (OK or Error)
+     * @throws AppException
+     */
     public static JsonObject deletePayment(Payment payment) throws AppException {
         JsonObject innerObject = new JsonObject();
 
