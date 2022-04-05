e837b89bceba6fdc8cae581c018b88b56118603e
tymchuk.volodymyr <tymchuk.volodymyr@gmail.com>
2022-04-05 13:33:28 +0300

    comments for RegistrationCommand

--- src/main/java/com/tvv/web/command/RegistrationCommand.java
+++ src/main/java/com/tvv/web/command/RegistrationCommand.java
@@ -9,16 +9,34 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
+/**
+ * Registration command
+ */
 public class RegistrationCommand extends Command {
 
     private static final Logger log = Logger.getLogger(RegistrationCommand.class);
 
+    /**
+     * Load registration page for new user
+     * @param request
+     * @param response
+     * @throws IOException
+     * @throws ServletException
+     */
     @Override
     public void executePost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-
+        RequestDispatcher disp = request.getRequestDispatcher(Path.PAGE__REGISTRATION);
+        disp.forward(request, response);
+        log.trace("Forward to: " + Path.PAGE__REGISTRATION);
     }
 
+    /**
+     * Load registration page for new user
+     * @param request
+     * @param response
+     * @throws IOException
+     * @throws ServletException
+     */
     @Override
     public void executeGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         RequestDispatcher disp = request.getRequestDispatcher(Path.PAGE__REGISTRATION);
