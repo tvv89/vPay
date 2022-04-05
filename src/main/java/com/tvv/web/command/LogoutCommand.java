592f07aee10c03dd27d361a113717da62a48387d
tymchuk.volodymyr <tymchuk.volodymyr@gmail.com>
2022-04-05 13:31:16 +0300

    comments for LogoutCommand

--- src/main/java/com/tvv/web/command/LogoutCommand.java
+++ src/main/java/com/tvv/web/command/LogoutCommand.java
@@ -11,22 +11,45 @@ import javax.servlet.http.HttpSession;
 import com.tvv.web.webutil.Path;
 import org.apache.log4j.Logger;
 
+/**
+ * Logout user command for authorisation user
+ */
 public class LogoutCommand extends Command {
 
 
 	private static final Logger log = Logger.getLogger(LogoutCommand.class);
 
+	/**
+	 * POST request function is same GET request function. Execute 'process'
+	 * @param request
+	 * @param response
+	 * @throws IOException
+	 * @throws ServletException
+	 */
 	@Override
 	public void executePost(HttpServletRequest request,
 							HttpServletResponse response) throws IOException, ServletException {
 		process(request,response);
 	}
 
+	/**
+	 * GET request function is same POST request function. Execute 'process'
+	 * @param request
+	 * @param response
+	 * @throws IOException
+	 * @throws ServletException
+	 */
 	@Override
 	public void executeGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		process(request,response);
 	}
 
+	/**
+	 * Function invalidate session and redirect to start page
+	 * @param request
+	 * @param response
+	 * @throws IOException
+	 */
 	void process (HttpServletRequest request, HttpServletResponse response) throws IOException {
 		log.debug("Command starts "+ request.getMethod());
 
