f5e37e49c5be441afa17c51c5388d319da418327
tymchuk.volodymyr <tymchuk.volodymyr@gmail.com>
2022-04-05 22:04:35 +0300

    comments for StringHash

--- src/main/java/com/tvv/utils/StringHash.java
+++ src/main/java/com/tvv/utils/StringHash.java
@@ -5,10 +5,22 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * Hash functions for password
+ */
 public class StringHash {
+
+    /**
+     * Create new Hash string
+     * @param input input string
+     * @return hash string
+     */
     public static String getHashString(String input) {
         String outputHash = "";
         try {
+            /**
+             * use SHA-256
+             */
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] encodedHash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
             BigInteger number = new BigInteger(1, encodedHash);
