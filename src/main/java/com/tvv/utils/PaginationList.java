33fcb00665bf55bedd041219ed860877f06d2e20
tymchuk.volodymyr <tymchuk.volodymyr@gmail.com>
2022-04-05 21:25:03 +0300

    comments for PaginationList

--- src/main/java/com/tvv/utils/PaginationList.java
+++ src/main/java/com/tvv/utils/PaginationList.java
@@ -3,8 +3,19 @@ package com.tvv.utils;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Pagination functions class
+ */
 public class PaginationList {
 
+    /**
+     * Create list per current page = 'page'
+     * @param tList input List type 'T'
+     * @param page page what we want to show
+     * @param count item per page
+     * @param <T> objects (User, Account, Payment, Card)
+     * @return List of object per 'page'
+     */
     public static <T> List<T> getListPage(List<T> tList, int page, int count) {
         List<T> result = new ArrayList<>();
         if (tList==null || tList.isEmpty()) return result;
@@ -18,6 +29,13 @@ public class PaginationList {
         return result;
     }
 
+    /**
+     * Get Max pages for input list
+     * @param tList List of object
+     * @param count items per page
+     * @param <T> type of object
+     * @return int value pages
+     */
     public static <T> int getPages(List<T> tList, int count) {
         return (int)Math.ceil((double)tList.size()/count);
     }
