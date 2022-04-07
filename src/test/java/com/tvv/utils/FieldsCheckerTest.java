bbeac83ae529ce64482f4b6d546f9aa0cd835b7b
tymchuk.volodymyr <tymchuk.volodymyr@gmail.com>
2022-04-07 12:02:30 +0300

    i18n action and function for application save user locale refactoring

---
+++ src/test/java/com/tvv/utils/FieldsCheckerTest.java
@@ -0,0 +1,36 @@
+package com.tvv.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FieldsCheckerTest {
+
+    @Test
+    void checkNameField() {
+    }
+
+    @Test
+    void checkEMailAddress() {
+    }
+
+    @Test
+    void checkPasswordField() {
+    }
+
+    @Test
+    void checkCardNumber() {
+    }
+
+    @Test
+    void checkBalanceDouble() {
+    }
+
+    @Test
+    void checkEqualsPassword() {
+    }
+
+    @Test
+    void checkAge18YearsOld() {
+    }
+}
